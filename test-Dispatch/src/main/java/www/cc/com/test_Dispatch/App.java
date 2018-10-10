27f9195f6b413fa7a4260d315076e2520b68cdd6
cc <ccluocong@foxmail.com>
2018-10-10 17:49:06 +0800

    add new maven projrct

---
+++ test-Dispatch/src/main/java/www/cc/com/test_Dispatch/App.java
@@ -0,0 +1,15 @@
+package www.cc.com.test_Dispatch;
+
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends SpringBootServletInitializer
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
